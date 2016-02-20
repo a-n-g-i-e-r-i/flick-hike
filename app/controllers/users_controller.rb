c2b2c5701470ddcef228f23333c2cacb96b05d6f
isangieri <isabelleangieri@gmail.com>
2016-02-19 16:34:04 -0800

    defined all users_controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in?, only: [ :edit, :update, :destroy]
+    before_action :logged_out?, only: [:new, :create]
 
   def index
     @users = User.all
@@ -10,7 +12,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    # login(@user)
+    login(@user)
     redirect_to @user
   end
 
@@ -19,12 +21,38 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find_by_id(params[:id])
+    unless current_user == @user
+      redirect_to user_path(@user)
+      flash[:notice] = "You can't edit that user"
+    end
   end
 
   def update
+    @user = User.find_by_id(params[:id])
+    if current_user == @user
+      if @user.update_attributes(user_params)
+        flash[:notice] = "Successfully updated user info"
+        redirect_to user_path(@user)
+      else
+        flash[:error] = @user.error.full_messages.join(", ")
+        redirect_to edit_user_path(@user)
+      end
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
-  def delete
+  def destroy
+    @user = User.find_by_id(params[:id])
+    if current_user == @user
+      @user.destroy
+      flash[:notice] = "Successfully deleted #{@user.username}"
+      redirect_to users_path
+    else
+      flash[:notice] = "You can't handle the truth!"
+      redirect_to user_path(@user)
+    end
   end
 
   private

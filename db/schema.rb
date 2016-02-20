300fb4319713a8f9b92b08fe8139407cdbfb9ae1
isangieri <isabelleangieri@gmail.com>
2016-02-19 16:46:37 -0800

    updated schema, controller and models with all changes, up to date

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219195029) do
+ActiveRecord::Schema.define(version: 20160219232707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
+
+  create_table "scenes", force: :cascade do |t|
+    t.string   "locations"
+    t.string   "title"
+    t.string   "writer"
+    t.string   "director"
+    t.string   "release_year"
+    t.string   "production_company"
+    t.string   "actor_1"
+    t.string   "actor_2"
+    t.string   "actor_3"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -25,4 +49,5 @@ ActiveRecord::Schema.define(version: 20160219195029) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "lists", "users"
 end

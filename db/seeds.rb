b048ff63375e422b77332276b52c18e7002f1bfa
isangieri <isabelleangieri@gmail.com>
2016-02-19 16:45:44 -0800

    added seed data for users, lists and scenes to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,39 @@
 include ActionView::Helpers
 
 User.destroy_all
+List.destroy_all
+Scene.destroy_all
 
-User.create ({
-  username: "bagel",
-  description: "Film lover | Owner of many bad romantic comedies | Lover of San Francisco | Like to walk around.",
-  email: "bagel@isangieri.com",
-  password: "yoyoyo"
-})
+1.times do
+  new_scene = Scene.create ({
+    locations: "Mason & California Streets, San Francisco, CA",
+    title: "180",
+    writer: "Umarji Anuradha, Jayendra, Aarthi Sriram, & Suba ",
+    director: "Jayendra",
+    release_year: "2011",
+    production_company: "SPI Cinemas",
+    actor_1: "Siddarth",
+    actor_2: "Nithya Menon",
+    actor_3: "Priya Anand"
+  })
+end
+
+1.times do
+  new_user = User.create ({
+    username: "bagel",
+    description: "Film lover | Owner of many bad romantic comedies | Lover of San Francisco | Like to walk around.",
+    email: "bagel@isangieri.com",
+    password: "yoyoyo"
+  })
+
+  3.times do
+    new_list = List.new
+    new_list.title = FFaker::HipsterIpsum.words(rand(1)+3).join(" ")
+    new_list.description = FFaker::Lorem.phrase
+    new_list.save
+    new_user.lists.push new_list
+  end
+end
 
 20.times do
 	user_params = Hash.new
@@ -25,5 +51,13 @@ User.create ({
 	user_params[:password]  = "123456"
 
 	new_user = User.create(user_params)
+
+  3.times do
+    new_list = List.new
+    new_list.title = FFaker::HipsterIpsum.words(rand(1)+3).join(" ")
+    new_list.description = FFaker::Lorem.phrase
+    new_list.save
+    new_user.lists.push new_list
+  end
 
 end

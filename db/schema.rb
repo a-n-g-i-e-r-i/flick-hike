464bc19bf161758bea910cc51374aba44469f1c5
isangieri <isabelleangieri@gmail.com>
2016-02-21 11:06:18 -0800

    schema.rb reflects currentdatabase

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219232707) do
+ActiveRecord::Schema.define(version: 20160221182827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "list_scenes", force: :cascade do |t|
+    t.integer  "scene_id"
+    t.integer  "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "list_scenes", ["list_id"], name: "index_list_scenes_on_list_id", using: :btree
+  add_index "list_scenes", ["scene_id"], name: "index_list_scenes_on_scene_id", using: :btree
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
@@ -49,5 +59,7 @@ ActiveRecord::Schema.define(version: 20160219232707) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "list_scenes", "lists"
+  add_foreign_key "list_scenes", "scenes"
   add_foreign_key "lists", "users"
 end

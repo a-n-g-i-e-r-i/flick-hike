d629d2de33517a4e9fa6bb8b55825c6191127deb
isangieri <isabelleangieri@gmail.com>
2016-02-21 12:31:15 -0800

    added nested attributes

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -3,4 +3,6 @@ class Scene < ActiveRecord::Base
   has_many :list_scenes, dependent: :destroy
   has_many :lists, through: :list_scenes
 
+  accepts_nested_attributes_for :list_scenes, :lists
+
 end

6cf29e1db325b005fe369557c83ebcf072833a8a
isangieri <isabelleangieri@gmail.com>
2016-02-22 14:03:08 -0800

    removed meddling validations

--- app/models/list_scene.rb
+++ app/models/list_scene.rb
@@ -2,7 +2,8 @@ class ListScene < ActiveRecord::Base
 
   belongs_to :scene
   belongs_to :list
-
-  validates_presence_of :scene, :list
+  
+  # be wary of these simultaneous validations on join tables
+  # validates_presence_of :scene, :list
 
 end

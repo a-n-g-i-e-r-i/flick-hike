33eda873c700acbb275e9315ba10b2bb4ed0a7a5
isangieri <isabelleangieri@gmail.com>
2016-02-22 14:04:44 -0800

    created routes for listscene index and create

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get "/scenes/:id", to: "scenes#show", as: "scene"
   get "/scenes/:id/list/new", to: "lists#new", as: "scene_new_list"
 
+  get "/listscenes", to: "listscenes#index", as: "list_scenes"
+  post "/listscenes", to: "listscenes#create", as: "create_listscene"
+
   post "/sessions", to: "sessions#create"
   get "/login", to: "sessions#new", as: "new_session"
   get "/logout", to: "sessions#destroy", as: "destroy_session"

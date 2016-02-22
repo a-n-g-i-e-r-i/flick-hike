d014e2414e36f10365192925b79b53fa5297f424
isangieri <isabelleangieri@gmail.com>
2016-02-22 13:05:17 -0800

    create listscene on show page

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -5,9 +5,16 @@ class ScenesController < ApplicationController
   end
 
   def show
+    @listscene = ListScene.new
     @scene = Scene.find_by_id(params[:id])
     if current_user
-      @user_lists = current_user.lists
+      @user_lists = []
+      @temp_lists = current_user.lists
+      @temp_lists.each do |a|
+        if !a.scenes.find_by_id(@scene.id)
+          @user_lists << a
+        end
+      end
     else
       @user_lists = ""
     end

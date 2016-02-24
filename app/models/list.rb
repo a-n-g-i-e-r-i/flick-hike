cf01f9597f6678a81b4fa120fcedb9010bfdfe63
isangieri <isabelleangieri@gmail.com>
2016-02-24 08:05:37 -0800

    added list validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,6 +6,20 @@ class List < ActiveRecord::Base
 
   accepts_nested_attributes_for :list_scenes, :scenes
 
+  validates :title, :description,
+  presence: true,
+  length: { maximum: 140 }
+
+  VALID_TITLE_REGEX = //
+    validates :title,
+      length: { minimum: 2 },
+      format: { with: VALID_TITLE_REGEX }
+
+  VALID_DESCRIPTION_REGEX = //
+    validates :description,
+      length: { minimum: 10 },
+      format: { with: VALID_DESCRIPTION_REGEX }
+
   default_scope {order("title ASC") }
 
   scope :title_like, -> (title) { where("title ilike ?", title)}

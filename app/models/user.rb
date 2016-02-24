5c40d629fe31b7a5d99400dc099a0199b6f4643e
isangieri <isabelleangieri@gmail.com>
2016-02-24 08:05:58 -0800

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,27 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  # Sets user's userName & email address to lowercase during account creation.
+  before_save { self.email = email.downcase }
+  before_save { self.username = username.downcase }
+
+  validates :username, :email, :password, :description,
+  presence: true,
+  length: { maximum: 140 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.\+]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+
+  validates :username,
+    uniqueness: { case_sensitive: false }
+
+  VALID_PASSWORD_REGEX = /[a-zA-Z0-9]+/
+    validates :password,
+      length: { minimum: 8 },
+      format: { with: VALID_PASSWORD_REGEX }
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])

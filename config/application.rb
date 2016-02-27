eafff85c487373ec8c38450699567e3022166112
isangieri <isabelleangieri@gmail.com>
2016-02-27 09:36:09 -0800

    added line to avoid precompilation of assets

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module FlickHike
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Avoid precompilation of assets
+    config.assets.precompile = []
   end
 end

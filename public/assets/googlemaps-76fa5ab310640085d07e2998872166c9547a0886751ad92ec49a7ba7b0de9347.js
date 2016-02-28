8a747e1baa619e0cb6ac6efd01e256ece715562e
isangieri <isabelleangieri@gmail.com>
2016-02-28 15:11:12 -0800

    clobbered/assets

--- public/assets/googlemaps-76fa5ab310640085d07e2998872166c9547a0886751ad92ec49a7ba7b0de9347.js
+++
@@ -1,6 +0,0 @@
-$.getScript('/assets/application.js',function(){
-  // calls for the ready function (first loading)
-  $(document).ready(ready);
-  // (secondary loadings)
-  $(document).on('page:load',ready);
-});

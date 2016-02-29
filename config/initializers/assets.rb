85ef887ceb06f69bf182e12078136705154534a8
isangieri <isabelleangieri@gmail.com>
2016-02-28 17:02:53 -0800

    clobber

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( autocomplete.js )
 Rails.application.config.assets.precompile += %w( googlemaps.js )
+Rails.application.config.assets.precompile += %w( application.js )

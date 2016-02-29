b8c95253749c6f4f871465e5d48732b81e7dcbb4
isangieri <isabelleangieri@gmail.com>
2016-02-28 17:14:02 -0800

    refactoring precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( autocomplete.js )
-Rails.application.config.assets.precompile += %w( googlemaps.js )
-Rails.application.config.assets.precompile += %w( application.js )
